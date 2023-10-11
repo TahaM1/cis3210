7621ec2cc1ce5233927ec7a48ada3d6735744b66
TahaM1 <tahanaeem12@hotmail.com>
2023-10-11 17:09:52 -0400

    file sending working

--- a1/TCPclient.c
+++ a1/TCPclient.c
@@ -9,47 +9,71 @@
 
 #define MAXRCVLEN 500
 #define PORTNUM 2300
- 
+#define DEFAULT_BUFSIZE 4096
+
+void error(const char *msg)
+{
+	perror(msg);
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[])
 {
-	char buffer[MAXRCVLEN + 1]; /* +1 so we can add null terminator */
-	char* msg = "Hello Worlds!\n";
-	int len, mysocket;
-	struct sockaddr_in dest; // socket info about the machine connecting to us
- 
-	/* Create a socket.
-	   The arguments indicate that this is an IPv4, TCP socket
-	*/
-	mysocket = socket(AF_INET, SOCK_STREAM, 0);
-  
-	memset(&dest, 0, sizeof(dest));                // zero the struct
-	
-	//Initialize the destination socket information
-	dest.sin_family = AF_INET;					   // Use the IPv4 address family
-	if (argc == 1){
-		dest.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // Set destination IP number - localhost, 127.0.0.1
-	}else if (argc == 2){
-		inet_aton(argv[1], &dest.sin_addr); // Set destination IP number from command line arg
-	}else{
-		printf("Invalid number of arguments\n");
-		printf("Usage: %s <ip>, where ip is an optional IP4 address in the dot-decimal notation\n", argv[0]);
-		printf("If the IP address is not proided, client attempts to connect to localhost\n");
-		exit(0);
+	if (argc < 4)
+	{
+		fprintf(stderr, "Usage: %s fileName server-IP-address:port-number [bufSize]\n", argv[0]);
+		exit(EXIT_FAILURE);
 	}
 
-	dest.sin_port = htons(PORTNUM);                // Set destination port number
- 	
-	// Connect to the server
-	connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in));
-  
-	send(mysocket, msg, strlen(msg), 0); 
-	len = recv(mysocket, buffer, MAXRCVLEN, 0);
- 
-	/* We have to null terminate the received data ourselves */
-	buffer[len] = '\0';
- 
-	printf("Received %s (%d bytes) from %s on port %d.\n", buffer, len, inet_ntoa(dest.sin_addr), ntohs(dest.sin_port));
- 
+	char *fileName = argv[1];
+	char *serverAddress = strtok(argv[2], ":");
+	char *serverPortStr = strtok(NULL, ":");
+	int serverPort = (serverPortStr != NULL) ? atoi(serverPortStr) : PORTNUM;
+	int bufSize = (argc == 5) ? atoi(argv[4]) : DEFAULT_BUFSIZE;
+
+	FILE *file = fopen(fileName, "rb");
+	if (file == NULL)
+	{
+		error("Error opening file");
+	}
+
+	char buffer[bufSize];
+	int bytesRead;
+
+	int mysocket = socket(AF_INET, SOCK_STREAM, 0);
+	if (mysocket == -1)
+	{
+		error("Error creating socket");
+	}
+
+	struct sockaddr_in dest;
+	memset(&dest, 0, sizeof(dest));
+	dest.sin_family = AF_INET;
+	dest.sin_port = htons(serverPort);
+
+	if (inet_pton(AF_INET, serverAddress, &(dest.sin_addr)) <= 0)
+	{
+		error("Error converting IP address");
+	}
+
+	if (connect(mysocket, (struct sockaddr *)&dest, sizeof(struct sockaddr_in)) == -1)
+	{
+		error("Error connecting to the server");
+	}
+
+	// Send the file in chunks
+	while ((bytesRead = fread(buffer, 1, bufSize, file)) > 0)
+	{
+		if (send(mysocket, buffer, bytesRead, 0) == -1)
+		{
+			error("Error sending data");
+		}
+	}
+
+	printf("File sent successfully.\n");
+
+	fclose(file);
 	close(mysocket);
+
 	return EXIT_SUCCESS;
 }
--- a1/TCPserver.c
+++ a1/TCPserver.c
@@ -9,16 +9,17 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <sys/socket.h>
- 
+
 #define PORTNUM 2300
 #define MAXRCVLEN 500
+#define DEFAULT_BUFSIZE 4096
 
-int mysocket;            // socket used to listen for incoming connections
+int mysocket; // socket used to listen for incoming connections
 int consocket;
 
-static void sigintCatcher(int signal,  siginfo_t* si, void *arg)
+static void sigintCatcher(int signal, siginfo_t *si, void *arg)
 {
-    printf("\n\n************** Caught SIG_INT: shutting down the server ********************\n");
+	printf("\n\n************** Caught SIG_INT: shutting down the server ********************\n");
 
 	close(consocket);
 	close(mysocket);
@@ -27,57 +28,64 @@ static void sigintCatcher(int signal,  siginfo_t* si, void *arg)
 
 int main(int argc, char *argv[])
 {
+	if (argc < 2)
+	{
+		fprintf(stderr, "Usage: %s port-number [bufSize]\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int serverPort = atoi(argv[1]);
+	int bufSize = (argc == 3) ? atoi(argv[2]) : DEFAULT_BUFSIZE;
+
 	int len;
-	char buffer[MAXRCVLEN + 1]; // +1 so we can add null terminator
-  
+	char buffer[bufSize + 1]; // +1 so we can add null terminator
+
 	struct sockaddr_in dest; // socket info about the machine connecting to us
 	struct sockaddr_in serv; // socket info about our server
 
-
 	// set up the sigint handler
 	struct sigaction signaler;
-    
-    memset(&signaler, 0, sizeof(struct sigaction));
-    sigemptyset(&signaler.sa_mask);
 
-    signaler.sa_sigaction = sigintCatcher;
-    signaler.sa_flags = SA_SIGINFO;
-    sigaction(SIGINT, &signaler, NULL);
+	memset(&signaler, 0, sizeof(struct sigaction));
+	sigemptyset(&signaler.sa_mask);
 
-	//Set up socket info
+	signaler.sa_sigaction = sigintCatcher;
+	signaler.sa_flags = SA_SIGINFO;
+	sigaction(SIGINT, &signaler, NULL);
 
+	// Set up socket info
 	socklen_t socksize = sizeof(struct sockaddr_in);
 
-	memset(&serv, 0, sizeof(serv));           // zero the struct before filling the fields
-	
-	serv.sin_family = AF_INET;                // Use the IPv4 address family
-	serv.sin_addr.s_addr = htonl(INADDR_ANY); // Set our address to any interface 
-	serv.sin_port = htons(PORTNUM);           // Set the server port number 
+	memset(&serv, 0, sizeof(serv));			  // zero the struct before filling the fields
+	serv.sin_family = AF_INET;				  // Use the IPv4 address family
+	serv.sin_addr.s_addr = htonl(INADDR_ANY); // Set our address to any interface
+	serv.sin_port = htons(serverPort);		  // Set the server port number
 
 	/* Create a socket.
-   	   The arguments indicate that this is an IPv4, TCP socket
+	   The arguments indicate that this is an IPv4, TCP socket
 	*/
 	mysocket = socket(AF_INET, SOCK_STREAM, 0);
-  
+
 	// bind serv information to mysocket
 	// Unlike all other function calls in this example, this call to bind()
 	// does some basic error handling
+	int flag = 1;
 
-	int flag=1;
-
-	if (setsockopt(mysocket,SOL_SOCKET,SO_REUSEADDR,&flag,sizeof(flag)) == -1) {
-    	printf("setsockopt() failed\n");
+	if (setsockopt(mysocket, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag)) == -1)
+	{
+		printf("setsockopt() failed\n");
 		printf("%s\n", strerror(errno));
-    	exit(1);
-	} 
+		exit(1);
+	}
 
-	if (bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)) != 0){
-		printf("Unable to open TCP socket on localhost:%d\n", PORTNUM);
+	if (bind(mysocket, (struct sockaddr *)&serv, sizeof(struct sockaddr)) != 0)
+	{
+		printf("Unable to open TCP socket on localhost:%d\n", serverPort);
 
-	/* 	strerror() returns a string representation of the system variable "errno"
-	 	errno is the integer code of the error that occured during the last system call from this process
-	 	need to include errno.h to use this function
-		*/
+		/*  strerror() returns a string representation of the system variable "errno"
+			errno is the integer code of the error that occurred during the last system call from this process
+			need to include errno.h to use this function
+			*/
 		printf("%s\n", strerror(errno));
 		close(mysocket);
 		return 0;
@@ -85,37 +93,44 @@ int main(int argc, char *argv[])
 
 	// start listening, allowing a queue of up to 1 pending connection
 	listen(mysocket, 0);
-	
+
 	// Create a socket to communicate with the client that just connected
 	consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
-  
-	while(consocket)
+
+	FILE *file = fopen("example.txt", "wb");
+	if (file == NULL)
 	{
-		//dest contains information - IP address, port number, etc. - in network byte order
-		//We need to conert it to host byte order before displaying it
+		perror("Error opening file for writing");
+		close(mysocket);
+		return EXIT_FAILURE;
+	}
+
+	while (consocket)
+	{
+		// dest contains information - IP address, port number, etc. - in network byte order
+		// We need to convert it to host byte order before displaying it
 		printf("Incoming connection from %s on port %d\n", inet_ntoa(dest.sin_addr), ntohs(dest.sin_port));
-		
+
 		// Receive data from the client
-		len = recv(consocket, buffer, MAXRCVLEN, 0);
-		buffer[len] = '\0';
-		printf("Received %s\n", buffer);
-		
-		//Convert data to upper case
-		int bufLen = strlen(buffer);
-		for(int i = 0; i < bufLen; i++){
-			buffer[i] = toupper(buffer[i]);
+		while ((len = recv(consocket, buffer, bufSize, 0)) > 0)
+		{
+			buffer[len] = '\0';
+			printf("Received %d bytes\n", len);
+
+			// Write received data to the file
+			fwrite(buffer, 1, len, file);
 		}
-		
-		//Send data to client
-		send(consocket, buffer, strlen(buffer), 0); 
-		
-		//Close current connection
+
+		fclose(file);
+
+		// Close current connection
 		close(consocket);
-		
-		//Continue listening for incoming connections
+
+		// Continue listening for incoming connections
 		consocket = accept(mysocket, (struct sockaddr *)&dest, &socksize);
 	}
 
 	close(mysocket);
+
 	return EXIT_SUCCESS;
 }
